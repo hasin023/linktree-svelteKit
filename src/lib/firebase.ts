45e33068a06c16a8340a842e696809465ea46d9f
Hasin <hasinmahtab.alvee@gmail.com>
2024-06-18 20:48:55 +0600

    Added User store with google provider auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, type User } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 // Change this to your firebase project configuration
 const firebaseConfig = {
@@ -18,3 +19,31 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+/**
+ *
+ * @returns A store with the currently singed in firebase user
+ *
+ */
+const userStore = () => {
+	let unsubscribe: () => void;
+
+	if (!auth || !globalThis.window) {
+		console.log("Auth is not initialized or not in browser");
+		const { subscribe } = writable<User | null>(null);
+
+		return { subscribe };
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+	});
+
+	return { subscribe };
+};
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+	import { auth } from '$lib/firebase';
+	import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+
+	import { user } from '$lib/firebase';
+
+	const singInWithGoogle = async () => {
+		const provider = new GoogleAuthProvider();
+		const user = await signInWithPopup(auth, provider);
+
+		console.log(user);
+	};
 </script>
 
-<h2>Login</h2>
+<h2 class="text-2xl mb-4">LinkTree</h2>
+
+{#if $user}
+	<h2 class="card-title">Welcome, {$user.displayName}</h2>
+	<p class="text-center text-success">You are logged in!</p>
+	<button on:click={() => signOut(auth)} class="btn btn-sm btn-outline btn-warning">Sign Out</button
+	>
+{:else}
+	<button on:click={singInWithGoogle} class="btn btn-outline btn-sm btn-accent"
+		>Sign in with Google</button
+	>
+{/if}
