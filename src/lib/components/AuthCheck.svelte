bf2b761f28c7d53f21e3461b52beebc83943a762
Hasin <hasinmahtab.alvee@gmail.com>
2024-06-18 21:13:15 +0600

    Added Auth check for routes

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { user } from '$lib/firebase';
+</script>
+
+{#if $user}
+	<slot />
+{:else}
+	<p class="text-error my-8 flex flex-col gap-4">
+		You must be signed in to view this page.
+		<a class="btn btn-sm btn-outline btn-secondary" href="/login">Sign in</a>
+	</p>
+{/if}
